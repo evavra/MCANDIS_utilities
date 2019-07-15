e412278bf3ded81b77e9a2f64cc0357e2c60e371
evavra <43979321+evavra@users.noreply.github.com>
2019-07-15 10:54:43 -0700

    Add files via upload

---
+++ CANDIS_utilities.py
@@ -0,0 +1,13 @@
+
+# Utility functions for running MCANDIS
+# Ellis Vavra, July 15, 2019
+
+def make_dates_to_use(intf_list):
+    origList = []
+    with open(intf_list) as file:
+        origList.append.readlines()
+
+    for pair in origList:
+        date1 = pair.split()
+
+
